88897025602aba47ce11fa06bae595be15c7b7a1
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-05-04 01:35:53 +0300

    Tetromino.rotate fixed

--- src/classes/tetromino.py
+++ src/classes/tetromino.py
@@ -2,40 +2,59 @@ from constants import *
 from pygame.math import Vector2
 import pygame
 from random import choice
+from numpy import rot90
 
 
 class Tetromino:
-	tetrominos = [
-		[Vector2(0, 0), Vector2(0, 1), Vector2(0, 2), Vector2(0, 3)],  # straight
-		[Vector2(0, 0), Vector2(1, 1), Vector2(0, 1), Vector2(1, 0)],  # square
-		[Vector2(0, 0), Vector2(0, 1), Vector2(0, 2), Vector2(1, 1)],  # T
-		[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(2, 1)],  # L
-		[Vector2(0, 1), Vector2(1, 0), Vector2(1, 1), Vector2(2, 0)]  # skew
-	]
+	tetrominos = dict(
+		straight=[
+			[0, 0, 0, 0],
+			[1, 1, 1, 1],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0]],
+		T=[
+			[0, 1, 0],
+			[1, 1, 1],
+			[0, 0, 0]],
+		L=[
+			[0, 0, 1],
+			[1, 1, 1],
+			[0, 0, 0]],
+		J=[
+			[1, 0, 0],
+			[1, 1, 1],
+			[0, 0, 0]],
+		S=[
+			[0, 1, 1],
+			[1, 1, 0],
+			[0, 0, 0]],
+		Z=[
+			[1, 1, 0],
+			[0, 1, 1],
+			[0, 0, 0]],
+		square=[
+			[1, 1],
+			[1, 1]]
+	)
 	
 	def __init__(self):
-		self.start = Vector2(DISPLAY_WIDTH // CELL_SIZE // 2, -1)
-		self.body = self.tetrominos[4]
+		self.position = Vector2(DISPLAY_WIDTH // CELL_SIZE // 2, -1)
+		self.body = choice(list(self.tetrominos.values()))
 	
 	def draw(self, display) -> None:
-		for cell in self.body:
-			x_pos = (cell.x + self.start.x) * CELL_SIZE
-			y_pos = (cell.y + self.start.y) * CELL_SIZE
-			cell_rect = pygame.Rect(x_pos, y_pos, CELL_SIZE, CELL_SIZE)
-			pygame.draw.rect(surface=display, color=WHITE, rect=cell_rect)
+		for i, row in enumerate(self.body):
+			for j, c in enumerate(row):
+				if c == 1:
+					x_pos = (i + self.position.x) * CELL_SIZE
+					y_pos = (j + self.position.y) * CELL_SIZE
+					cell_rect = pygame.Rect(x_pos, y_pos, CELL_SIZE, CELL_SIZE)
+					pygame.draw.rect(surface=display, color=WHITE, rect=cell_rect)
 	
 	def move_down(self) -> None:
-		self.start += Vector2(0, 1)
+		self.position += Vector2(0, 1)
 	
 	def move_horizontally(self, side: Vector2) -> None:
-		for cell in self.body:
-			cell += side
+		self.position += side
 	
 	def rotate(self) -> None:
-		print("rotating...")
-		pivot = self.body[1]
-		print(f"my pivot is at {pivot.x} // {pivot.y}")
-		for cell in self.body:
-			detla_x = cell.x - pivot.x
-			delta_y = cell.y - pivot.y
-			print(detla_x, delta_y)
+		self.body = rot90(self.body)
