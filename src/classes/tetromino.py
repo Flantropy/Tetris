bc9c2b1885503c41899682cbe144d205b686ee7c
Flantropy <80071496+flantropy@users.noreply.github.com>
2021-05-05 23:00:58 +0300

    movement method updated
    Signed-off-by: Flantropy <80071496+Flantropy@users.noreply.github.com>

--- src/classes/tetromino.py
+++ src/classes/tetromino.py
@@ -6,55 +6,64 @@ from numpy import rot90
 
 
 class Tetromino:
-	tetrominos = dict(
-		straight=[
-			[0, 0, 0, 0],
-			[1, 1, 1, 1],
-			[0, 0, 0, 0],
-			[0, 0, 0, 0]],
-		T=[
-			[0, 1, 0],
-			[1, 1, 1],
-			[0, 0, 0]],
-		L=[
-			[0, 0, 1],
-			[1, 1, 1],
-			[0, 0, 0]],
-		J=[
-			[1, 0, 0],
-			[1, 1, 1],
-			[0, 0, 0]],
-		S=[
-			[0, 1, 1],
-			[1, 1, 0],
-			[0, 0, 0]],
-		Z=[
-			[1, 1, 0],
-			[0, 1, 1],
-			[0, 0, 0]],
-		square=[
-			[1, 1],
-			[1, 1]]
-	)
-	
-	def __init__(self):
-		self.position = Vector2(DISPLAY_WIDTH // CELL_SIZE // 2, -1)
-		self.body = choice(list(self.tetrominos.values()))
-	
-	def draw(self, display) -> None:
-		for i, row in enumerate(self.body):
-			for j, c in enumerate(row):
-				if c == 1:
-					x_pos = (i + self.position.x) * CELL_SIZE
-					y_pos = (j + self.position.y) * CELL_SIZE
-					cell_rect = pygame.Rect(x_pos, y_pos, CELL_SIZE, CELL_SIZE)
-					pygame.draw.rect(surface=display, color=WHITE, rect=cell_rect)
-	
-	def move_down(self) -> None:
-		self.position += Vector2(0, 1)
-	
-	def move_horizontally(self, side: Vector2) -> None:
-		self.position += side
-	
-	def rotate(self) -> None:
-		self.body = rot90(self.body)
+    tetrominos = dict(
+        straight=[
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]],
+        T=[
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]],
+        L=[
+            [0, 0, 1],
+            [1, 1, 1],
+            [0, 0, 0]],
+        J=[
+            [1, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]],
+        S=[
+            [0, 1, 1],
+            [1, 1, 0],
+            [0, 0, 0]],
+        Z=[
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0]],
+        square=[
+            [1, 1],
+            [1, 1]]
+    )
+    
+    def __init__(self):
+        self.__position = Vector2(DISPLAY_WIDTH // CELL_SIZE // 2, -1)
+        self.body = self.tetrominos["straight"]  # choice(list(self.tetrominos.values()))
+    
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value: Vector2):
+        print(f"setting value = {value}")
+        if 0 <= value.x:
+            self.__position = value
+        else:
+            self.__position = Vector2(0, value.y)
+            
+    def draw(self, display) -> None:
+        for i, row in enumerate(self.body):
+            for j, c in enumerate(row):
+                if c == 1:
+                    x_pos = (i + self.position.x) * CELL_SIZE
+                    y_pos = (j + self.position.y) * CELL_SIZE
+                    cell_rect = pygame.Rect(x_pos, y_pos, CELL_SIZE, CELL_SIZE)
+                    pygame.draw.rect(surface=display, color=WHITE, rect=cell_rect)
+    
+    def move(self, direction: Vector2) -> None:
+        self.position += direction
+    
+    def rotate(self) -> None:
+        self.body = rot90(self.body)
--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from classes import Game
 
 
 def main():
-	game = Game()
-	game.run()
-	
+    game = Game()
+    game.run()
+
 
 if __name__ == "__main__":
-	main()
+    main()
